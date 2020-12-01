7ea0f782c30a1f4820fa3c22a52cb44b0008a308
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-12-01 16:56:25 -0500

    Day 1

--- src/day_01/day01.rs
+++ src/day_01/day01.rs
@@ -4,11 +4,8 @@ use std::{fs::File, io::BufReader};
 pub fn challenge_01() -> usize {
     let input_file = File::open("./src/day_01/input.txt").unwrap();
     let input = BufReader::new(input_file);
-    let numbers: Vec<usize> = input
-        .lines()
-        .map(|s| s.unwrap().parse().unwrap())
-        .collect();
-    //println!("{:?}", numbers);
+    let numbers: Vec<usize> = input.lines().map(|s| s.unwrap().parse().unwrap()).collect();
+    //println!("Vector of numbers: {:?}", numbers);
 
     let mut prod = 0;
     'outer: for num1 in &numbers {
