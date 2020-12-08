4a15a3e60eaa63faa248086c4cb333ab2842b275
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-12-07 20:51:21 -0500

    Day 7

--- src/day_07/day07.rs
+++ src/day_07/day07.rs
@@ -2,6 +2,13 @@ use std::io::prelude::*;
 use std::{fs::File, io::BufReader};
 
 pub fn challenge_01() -> usize {
+    let input_file = File::open("./src/day_07/input.txt").unwrap();
+    let input = BufReader::new(input_file);
+    let lines = input.lines();
+
+    for line in lines {
+        println!("{}", line.unwrap());
+    }
     7
 }
 
