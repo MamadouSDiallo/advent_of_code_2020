8f9519fd47c982ac81ca5f64e9b2012ac244f8df
Mamadou S Diallo <msdiallo@quantifyafrica.org>
2020-12-16 01:41:38 -0500

    day 12

--- src/day_12/day12.rs
+++ src/day_12/day12.rs
@@ -194,8 +194,124 @@ pub fn challenge_01() -> usize {
         //     println!("{:?}", value);
         // }
     }
-    println!("{:?}", ferry);
+    // println!("{:?}", ferry);
     ferry.position.east + ferry.position.west + ferry.position.north + ferry.position.south
+}
+
+#[derive(Debug)]
+struct Waypoint {
+    position: Position,
+}
+
+impl Waypoint {
+    fn new() -> Self {
+        Self {
+            position: Position {
+                north: 1,
+                south: 0,
+                east: 10,
+                west: 0,
+            },
+        }
+    }
+
+    fn move_waypoint(&mut self, action: char, value: usize) {
+        if action == 'N' {
+            if self.position.south >= value {
+                self.position.south -= value;
+            } else if self.position.south > 0 {
+                self.position.north = value - self.position.south;
+                self.position.south = 0;
+            } else {
+                self.position.north += value;
+            }
+        } else if action == 'S' {
+            if self.position.north >= value {
+                self.position.north -= value;
+            } else if self.position.north > 0 {
+                self.position.south = value - self.position.north;
+                self.position.north = 0;
+            } else {
+                self.position.south += value;
+            }
+        } else if action == 'E' {
+            if self.position.west >= value {
+                self.position.west -= value;
+            } else if self.position.west > 0 {
+                self.position.east = value - self.position.west;
+                self.position.west = 0;
+            } else {
+                self.position.east += value;
+            }
+        } else if action == 'W' {
+            if self.position.east >= value {
+                self.position.east -= value;
+            } else if self.position.east > 0 {
+                self.position.west = value - self.position.east;
+                self.position.east = 0;
+            } else {
+                self.position.west += value;
+            }
+        } else if action == 'L' {
+            match value {
+                90 => {
+                    let temp = self.position.east;
+                    self.position.east = self.position.south;
+                    self.position.south = self.position.west;
+                    self.position.west = self.position.north;
+                    self.position.north = temp;
+                }
+                180 => {
+                    let temp1 = self.position.east;
+                    self.position.east = self.position.west;
+                    self.position.west = temp1;
+                    let temp2 = self.position.north;
+                    self.position.north = self.position.south;
+                    self.position.south = temp2;
+                }
+                270 => {
+                    let temp = self.position.east;
+                    self.position.east = self.position.north;
+                    self.position.north = self.position.west;
+                    self.position.west = self.position.south;
+                    self.position.south = temp;
+                }
+                _ => {
+                    panic!("Should never happen");
+                }
+            }
+        } else if action == 'R' {
+            match value {
+                90 => {
+                    let temp = self.position.east;
+                    self.position.east = self.position.north;
+                    self.position.north = self.position.west;
+                    self.position.west = self.position.south;
+                    self.position.south = temp;
+                }
+                180 => {
+                    let temp1 = self.position.east;
+                    self.position.east = self.position.west;
+                    self.position.west = temp1;
+                    let temp2 = self.position.north;
+                    self.position.north = self.position.south;
+                    self.position.south = temp2;
+                }
+                270 => {
+                    let temp = self.position.east;
+                    self.position.east = self.position.south;
+                    self.position.south = self.position.west;
+                    self.position.west = self.position.north;
+                    self.position.north = temp;
+                }
+                _ => {
+                    panic!("Should never happen");
+                }
+            }
+        } else {
+            panic!("action not valid!");
+        }
+    }
 }
 
 pub fn challenge_02() -> usize {
@@ -203,21 +319,68 @@ pub fn challenge_02() -> usize {
     let input = BufReader::new(input_file);
     let lines = input.lines();
 
-    let mut ferry = Ferry::new();
-    let mut waypoint = Ferry::new();
-    waypoint.position.east = 10;
-    waypoint.position.north = 1;
+    let mut waypoint = Waypoint::new();
+    let mut north = 0;
+    let mut south = 0;
+    let mut east = 0;
+    let mut west = 0;
 
     for line in lines {
         let item = line.as_ref().unwrap();
         let action = item.chars().nth(0).unwrap();
         let value = item[1..].parse::<usize>().unwrap();
 
-        if action == 'N' || action == 'S' || action == 'E' || action == 'W' {
-            ferry.move_ferry(action, value);
-            waypoint.move_ferry(action, value);
-        } else if action == 'L' {
-            
+        if action == 'N'
+            || action == 'S'
+            || action == 'E'
+            || action == 'W'
+            || action == 'L'
+            || action == 'R'
+        {
+            waypoint.move_waypoint(action, value);
+        } else if action == 'F' {
+            if waypoint.position.east > 0 {
+                let jump = value * waypoint.position.east;
+                if west >= jump {
+                    west -= jump;
+                } else if west > 0 {
+                    east = jump - west;
+                    west = 0;
+                } else {
+                    east += jump;
+                }
+            } else {
+                let jump = value * waypoint.position.west;
+                if east >= jump {
+                    east -= jump;
+                } else if east > 0 {
+                    west = jump - east;
+                    east = 0;
+                } else {
+                    west += jump;
+                }
+            }
+            if waypoint.position.north > 0 {
+                let jump = value * waypoint.position.north;
+                if south >= jump {
+                    south -= jump;
+                } else if south > 0 {
+                    north = jump - south;
+                    south = 0;
+                } else {
+                    north += jump;
+                }
+            } else {
+                let jump = value * waypoint.position.south;
+                if north >= jump {
+                    north -= jump;
+                } else if north > 0 {
+                    south = jump - north;
+                    north = 0;
+                } else {
+                    south += jump;
+                }
+            }
         }
 
         // if action == 'R' {
@@ -225,6 +388,6 @@ pub fn challenge_02() -> usize {
         //     println!("{:?}", value);
         // }
     }
-    println!("{:?}", ferry);
-    12
+    println!("{}, {}, {}, {}", south, north, east, west);
+    south + north + east + west
 }
